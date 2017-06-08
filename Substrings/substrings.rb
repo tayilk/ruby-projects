8971e1d72da163b4a641e6a5b016a46d46ec82fa
tayilk <tayilk@gmail.com>
2017-06-08 14:08:18 -0500

    completed substrings

---
+++ Substrings/substrings.rb
@@ -0,0 +1,41 @@
+def substrings(word, dictionary)
+
+	word_arr = word.split(" ")
+
+	word_arr.each do |i|
+		i.downcase!
+	end
+
+	word_count = Hash.new(1)
+
+	dictionary.each do |current_word|
+		
+		current_word.downcase!
+
+		word_arr.each do |check_word|
+
+			if check_word.include?(current_word)
+
+				if word_count.include?(current_word)
+
+					word_count[current_word] += 1
+
+				else
+
+					word_count[current_word] = 1
+
+				end
+
+			end
+
+		end
+
+	end
+
+	puts word_count
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+substrings("below", dictionary)
