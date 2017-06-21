1da85ece82f208d3886817c4d5a011b2132d4c84
tayilk <tayilk@gmail.com>
2017-06-21 16:46:03 -0500

    Implemented Bubble Sort By

--- Bubble-Sort/bubble_sort.rb
+++ Bubble-Sort/bubble_sort.rb
@@ -11,10 +11,31 @@ def bubble_sort(arr)
 		end
 		l = down
 	end
-
 	arr
 end
 
 sort_this = [2, 6, 7, 3, 2, 5, 8, 7, 5]
 
 p bubble_sort(sort_this)
+
+def bubble_sort_by(arr)
+	l = arr.length
+
+	until l == 0
+		down = 0
+		1.upto(l - 1) do |i|
+			if (yield arr[i - 1], arr[i]) > 0
+				arr[i - 1], arr[i] = arr[i], arr[i - 1]
+				down = i
+			end
+		end
+		l = down
+	end
+	arr
+end
+
+x = bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+	left.length - right.length
+end
+
+p x
