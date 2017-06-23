9bbf0db943e17da0e0b66107b875df118172e9bb
tayilk <tayilk@gmail.com>
2017-06-23 12:16:33 -0500

    completed my_inject

--- Enumerable/Enumerable.rb
+++ Enumerable/Enumerable.rb
@@ -1,4 +1,5 @@
 module Enumerable
+
 	def my_each
 		x = 0
 
@@ -89,4 +90,55 @@ module Enumerable
 		end
 		return count
 	end
+
+	def my_map(enum=nil)
+		x = 0
+		new_arr = []
+
+		while x < self.size
+			if enum
+				new_arr.push(enum)
+				x += 1
+			else
+				if yield(self[x])
+					new_arr.push(self[x])
+				end
+				x += 1
+			end
+		end
+		new_arr
+	end
+
+	def my_inject(first=0, second=nil)
+		sum = first
+		x = 0
+
+		if block_given?
+			while x < self.size
+				sum = yield(sum, self[x])
+				x += 1
+			end
+		elsif first.instance_of?(Symbol)
+			sum = 1
+			sum = 0 unless first == :* || first == :/
+			sym = first.to_s
+
+			while x < self.size
+				sum = sum.send(sym, self[x])
+				x += 1
+			end
+		elsif second
+			sym = second.to_s
+
+			while x < self.size
+				sum = sum.send(sym, self[x])
+				x += 1
+			end
+		end
+		sum
+	end
+end
+
+def multiply_els(arr)
+	arr.my_inject(:*)
 end
